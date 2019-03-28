5e54092819ef2eabcc068dbdb2d09a7f221ba271
George Theofilis <g.theofilis@clmsuk.com>
2019-03-28 12:44:09 +0000

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot-subscribe-service',
-    version='0.0.1',
+    version='0.1.0',
     zip_safe=False,
 )
