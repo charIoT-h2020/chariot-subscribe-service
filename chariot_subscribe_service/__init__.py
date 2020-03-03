4c7c5432a1db12f7c659077c1e31618f06d0712f
George Theofilis <theofilis.g@gmail.com>
2020-03-03 10:26:58 +0000

    Bump version: 0.2.0 → 0.11.0

--- chariot_subscribe_service/__init__.py
+++ chariot_subscribe_service/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.2.0'
+__version__ = '0.11.0'
 __service_name__ = 'subscriber-http-service'
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot-subscribe-service',
-    version='0.2.0',
+    version='0.11.0',
     zip_safe=False,
 )
