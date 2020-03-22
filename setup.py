884cb5e0ccac6036d0ff5951af621a6a1ee04cd9
George Theofilis <theofilis.g@gmail.com>
2020-03-22 21:14:26 +0000

    Edit package req

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 requirements = [
     'falcon',
     'influxdb',
-    'chariot_base==0.9.3',
+    'chariot_base',
     'paho-mqtt',
     'asyncio',
     'gmqtt',
